5b0244a0dd9d1098ad42e8f629d438237d2638a1
Amiya004 <adash8123@gmail.com>
2025-10-20 00:24:03 +0530

    Swapping of 2 numbers using 3rd variable

--- SwappingNumbers.java
+++ SwappingNumbers.java
@@ -13,14 +13,15 @@ public class SwappingNumbers {
         System.out.println("Provide your second number:");
         double num2 = scanner.nextDouble();
 
-        System.out.println("Before Swapping the value of the first number is "+num1+" and the value of second number is= "+num2);
+        System.out.println("Before Swapping the value of the first number is= "+num1+" and the value of second number is= "+num2);
 
         double temp = num1;
         num1 = num2;
         num2 = temp;
 
-        System.out.println("After Swapping the value of the first number is "+num1+" and the value of second number is= "+num2);
+        System.out.println("After Swapping the value of the first number is= "+num1+" and the value of second number is= "+num2);
 
+        scanner.close();
     }
 }
 
