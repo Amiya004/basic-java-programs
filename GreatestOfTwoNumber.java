e8176acbce026eed0940bf171153229ee6fabc82
Amiya004 <adash8123@gmail.com>
2025-10-20 19:18:32 +0530

    Find greatest between 2 numbers

---
+++ GreatestOfTwoNumber.java
@@ -0,0 +1,23 @@
+// Program to find Greatest number among 2 numbers.
+
+import java.util.Scanner;
+
+public class GreatestOfTwoNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter 1st number : ");
+        int num1 = scanner.nextInt();
+
+        System.out.print("Enter 2nd number : ");
+        int num2 = scanner.nextInt();
+
+        if(num1 > num2){
+            System.out.println(num1 + " is greatest");
+        } else {
+            System.out.println(num2 + " is greatest");
+        }
+
+        scanner.close();
+    }
+}
