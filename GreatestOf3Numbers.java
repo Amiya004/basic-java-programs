dbfc9250b7b70f08c25b32ea7e82e9e5388f15f3
Amiya004 <adash8123@gmail.com>
2025-10-20 19:32:13 +0530

    Greatest number between 3 numbers

---
+++ GreatestOf3Numbers.java
@@ -0,0 +1,31 @@
+// Program to find the Greatest number among 3 numbers. 
+
+import java.util.Scanner;
+
+public class GreatestOf3Numbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter 1st number : ");
+        int num1 = sc.nextInt();
+
+        System.out.println("Enter 2nd number : ");
+        int num2 = sc.nextInt();
+
+        System.out.println("Enter 3rd number : ");
+        int num3 = sc.nextInt();
+
+        if(num1 >= num2 && num1 >= num3) {
+            System.out.println();
+            System.out.println(num1 + " is greatest");
+        } else if(num2 >= num3){
+            System.out.println();
+            System.out.println(num2 + " is greatest");
+        } else {
+            System.out.println();
+            System.out.println(num3 + " is greatest");
+        }
+
+        sc.close();
+    }
+}
